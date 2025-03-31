d4b09fb16c8fdb33c7580c5e59df9a8fa9bc53cc
ltuominen <tuominenlj@gmail.com>
2025-03-31 16:23:05 -0400

    add code for petsurfer

--- code/preproc_pet.sh
+++ code/preproc_pet.sh
@@ -22,18 +22,24 @@ mri_vol2vol --reg ${pet_dir}/p2mri.reg.lta --mov ${pet_dir}/sum_mc_${pet_file} -
 python create_cs_roi.py ${subject}
 
 
-
+# these are for testing 
 pet_dir=/home/lauri/Documents/SVT1/test_subject
 pet_file=pet.nii.gz
 
-# run the mri_gtmpvc twice to get a) ref tac and b) high binding nii.gz 
+# run the mri_gtmpv to get high binding nii.gz 
 mri_gtmpvc --i ${pet_dir}/mc_${pet_file} --reg ${pet_dir}/p2mri.reg.lta --seg gtmseg.mgz \
 	--default-seg-merge  --auto-mask 1.01 --no-tfe --o ${pet_dir}/gtmpvc_hb.output \
 	--km-hb 11 12 13 50 51 52 --no-rescale --max-threads-minus-1
 
-mri_segstats --i ${pet_dir}/mc_${pet_file} --reg ${pet_dir}/p2mri.reg.lta --seg $SUBJECTS_DIR/test_subject/mri/cs_eroded.nii.gz --id 1 --avgwf ${pet_dir}/ref.km.txt
+# use mri_segstats to get ref tac
+mri_segstats --i ${pet_dir}/mc_${pet_file} --reg ${pet_dir}/p2mri.reg.lta \
+	--seg $SUBJECTS_DIR/test_subject/mri/cs_eroded.nii.gz --id 1 --avgwf ${pet_dir}/ref.km.txt
  
+# fit MRTM1 
+mri_glmfit --y ${pet_dir}/gtmpvc_hb.output/km.hb.tac.nii.gz --mrtm1 ${pet_dir}/ref.km.txt /home/lauri/Documents/SVT1/info/time.dat --o ${pet_dir}/mrtm1 --no-est-fwhm --nii.gz
 
-mri_gtmpvc --i ${pet_dir}/mc_${pet_file} --reg ${pet_dir}/p2mri.reg.lta --seg cs_eroded.nii.gz \
-	--default-seg-merge  --auto-mask 1.01 --no-tfe --o ${pet_dir}/gtmpvc_ref.output \
-	--km-ref 1 --no-rescale --max-threads-minus-1
+# get k2p from mrtm1 
+k2p=$(cat ${pet_dir}/mrtm1/k2prime.dat)
+
+# fit MRTM2 
+mri_glmfit --y gtm.nii.gz --mrtm2 ${pet_dir}/ref.km.txt /home/lauri/Documents/SVT1/info/time.dat $k2p --o ${pet_dir}/mrtm2 --no-est-fwhm --nii.gz
