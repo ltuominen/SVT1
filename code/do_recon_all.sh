5e2c31ea1b6bc0229b06b0a5447dd86c02f17bde
ltuominen <tuominenlj@gmail.com>
2025-04-02 15:19:42 -0400

    carrot

--- code/do_recon_all.sh
+++ code/do_recon_all.sh
@@ -2,5 +2,5 @@ export FREESURFER_HOME=/usr/local/freesurfer/8.0.0
 export SUBJECTS_DIR=/home/lauri/Documents/SVT1/SUBJECTS_DIR
 subject=$1
 path_to_T1w=$2
-#recon-all -i ${path_to_T1w} -s ${subject} -all -threads 8
+recon-all -i ${path_to_T1w} -s ${subject} -all -threads 8
 gtmseg --s ${subject} --samseg
